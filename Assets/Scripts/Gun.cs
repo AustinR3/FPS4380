e48f3d87572c6524f1a3e782d2c64922cb1a25a5
Austin Radhs <austinradhs@oakland.edu>
2023-04-06 12:11:43 -0400

    Added Gun To Shoot

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StarterAssets;
+
+public class Gun : MonoBehaviour
+{
+
+    private StarterAssetsInputs _input;
+    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject bulletPrefab;
+
+    [SerializeField]
+    private GameObject bulletPoint;
+
+    private float bulletSpeed = 600;
+    void Start()
+    {
+        _input = transform.root.GetComponent<StarterAssetsInputs>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_input.shoot)
+        {
+            Shoot();
+            _input.shoot = false;
+        }
+    }
+
+    void Shoot()
+    {
+        Debug.Log("Shoot");
+        GameObject bullet = Instantiate(bulletPrefab, bulletPoint.transform.position, transform.rotation);
+        bullet.GetComponent<Rigidbody>().AddForce(transform.forward * bulletSpeed);
+        Destroy(bullet, 1);
+    }
+}
